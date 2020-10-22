23f6072693d7ed19bac3c79c26fd3aeb985e338b
Gabriel Stellini <gabrielstellini@allfront.io>
2020-10-23 00:00:31 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Task } from './models/task.model';
+import {Observable} from 'rxjs';
+import {TasksService} from './services/tasks.service';
+import {switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'serviceworker-demo';
+export class AppComponent  implements OnInit {
+  newTask: Task = { name: null, isComplete: false };
+
+  allTasks$: Observable<Task[]>;
+
+  constructor(public tasksService: TasksService) {}
+
+  ngOnInit(): void {
+    this.allTasks$ = this.getTasks$();
+  }
+
+  getTasks$(): Observable<Task[]> {
+    return this.tasksService.getTasks();
+  }
+
+  createTask(): void {
+    this.allTasks$ = this.tasksService.createTask(this.newTask).pipe(
+      tap(() => this.newTask.name = null),
+      switchMap(() => this.getTasks$()),
+    );
+  }
+
+  toggleTask(task: Task): void {
+    const updatedTask: Task = {...task, isComplete: !task.isComplete};
+    this.allTasks$ = this.tasksService.toggleTask(updatedTask).pipe(
+      switchMap(() => this.getTasks$()),
+    );
+  }
+
+  deleteTask(id: number): void {
+    this.allTasks$ = this.tasksService.deleteTask(id).pipe(
+      switchMap(() => this.getTasks$()),
+    );
+  }
+
+  onFormChange($event: { target: { value: string }}): void {
+    this.newTask.name = $event.target.value;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  endpoint: 'http://localhost:3000/',
   production: true
 };
